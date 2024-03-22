2854d16ab195b6d3ee9f290b765b899c07a44299
ilosz01-ThinkPad <ilonaszczot@gmail.com>
2024-03-22 11:34:08 +0100

    Added my_new_analysis folder with scripts that read raw fastq files and implement NBIS suggestions

---
+++ my_new_analysis/config.py
@@ -0,0 +1,6 @@
+import os
+
+local_path = '/home/ilosz01/Documents/single_cell_sequencing'
+data_path = os.path.join(local_path, 'data')
+figures_path = os.path.join(local_path, 'figures')
+output_path = os.path.join(local_path, 'output')
